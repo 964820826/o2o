ced90c4f29f086db74fc88a4467c4b8d6e75e0fe
Office <964820826@qq.com>
2019-10-10 17:58:34 +0800

    返回结果类和统一异常处理

--- src/main/java/com/dyl/o2o/controller/shopadmin/ShopManagementController.java
+++ src/main/java/com/dyl/o2o/controller/shopadmin/ShopManagementController.java
@@ -1,12 +1,17 @@
 package com.dyl.o2o.controller.shopadmin;
 
 import com.dyl.o2o.core.exception.Result;
+import com.dyl.o2o.domain.ShopDo;
+import com.dyl.o2o.util.HttpRequestUtil;
+import com.dyl.o2o.util.R;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
 
 /** 店铺管理controller
  * @author ：dyl
@@ -17,7 +22,12 @@ import javax.servlet.http.HttpServletRequest;
 public class ShopManagementController {
 
     @RequestMapping(value = "/shop", method = RequestMethod.POST)
-    private Result registerShop(HttpServletRequest request){
+    private R registerShop(HttpServletRequest request){
+        //接收并转化参数，包括店铺信息和图片信息
+        String shopStr = HttpRequestUtil.getString(request,"shopStr");
+        ObjectMapper mapper = new ObjectMapper();
+        ShopDo shopDo = null;
+        shopDo = mapper.readValue(shopStr, ShopDo.class);
 
     }
 }
